6b24708d576e51a81bb5e9356cf8ad0672d0c787
ismailo-git <hassanliamsi@gmail.com>
2020-05-16 20:16:52 +0200

    Initialisation du projet

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADjiP:Diaspo djiboutienne</title>
+    <!--Link to custom CSS-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
