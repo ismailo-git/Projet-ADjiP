94352a8468d6193171a1a92fd9b295a9e8656b32
ismailo-git <hassanliamsi@gmail.com>
2020-05-17 18:31:45 +0200

    Modification des elements de la liste de navigation

--- index.php
+++ index.php
@@ -22,10 +22,17 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="#">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Politique</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Memmbres</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
         </div>
